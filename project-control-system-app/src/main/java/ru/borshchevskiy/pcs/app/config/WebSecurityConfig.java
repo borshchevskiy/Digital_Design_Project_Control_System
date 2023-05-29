9e15c083b3ffb1d0dddba38ff1bac8fa48e41e6a
Ilya Borshchevskiy <ilya.borshchevskiy@gmail.com>
2023-05-29 09:51:25 +0300

    Updated swagger config, code clean-up

--- project-control-system-app/src/main/java/ru/borshchevskiy/pcs/app/config/WebSecurityConfig.java
+++ project-control-system-app/src/main/java/ru/borshchevskiy/pcs/app/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package ru.borshchevskiy.pcs.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -21,15 +22,17 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeHttpRequests( (requests) ->
-                       requests.requestMatchers("/api/auth/login",
+                .authorizeHttpRequests((requests) ->
+                        requests.requestMatchers("/api/auth/login",
                                         "/api/auth/register",
                                         "/swagger-ui/**",
                                         "/v3/api-docs/**").permitAll()
                                 .anyRequest().authenticated())
                 .httpBasic();
+
         return http.build();
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
